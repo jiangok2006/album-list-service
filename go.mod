d7b0b23bb0adb0867a3d8c9ded45cf741bd0f932
jiangok2006 <jiangok2006@gmail.com>
2024-06-17 23:31:48 -0700

    first commit

---
+++ go.mod
@@ -0,0 +1,26 @@
+module github.com/jiangok2006/album_list_service
+
+go 1.22.4
+
+require (
+	buf.build/gen/go/jiangok/buf-hello/grpc/go v1.3.0-20240617023842-d17ffc790b20.3
+	buf.build/gen/go/jiangok/buf-hello/protocolbuffers/go v1.34.2-20240617023842-d17ffc790b20.2
+	github.com/grpc-ecosystem/grpc-gateway v1.16.0
+	google.golang.org/grpc v1.64.0
+)
+
+require (
+	github.com/golang/protobuf v1.5.4 // indirect
+	google.golang.org/genproto v0.0.0-20240227224415-6ceb2ff114de // indirect
+	google.golang.org/genproto/googleapis/api v0.0.0-20240513163218-0867130af1f8 // indirect
+	google.golang.org/protobuf v1.34.2 // indirect
+)
+
+require (
+	buf.build/gen/go/jiangok/buf-hello/grpc-ecosystem/gateway/v2 v2.20.0-20240617023842-d17ffc790b20.1
+	github.com/grpc-ecosystem/grpc-gateway/v2 v2.20.0 // indirect
+	golang.org/x/net v0.23.0 // indirect
+	golang.org/x/sys v0.18.0 // indirect
+	golang.org/x/text v0.15.0 // indirect
+	google.golang.org/genproto/googleapis/rpc v0.0.0-20240513163218-0867130af1f8 // indirect
+)
---
+++ main.go
@@ -0,0 +1,69 @@
+package main
+
+import (
+	"context"
+	"log"
+	"net/http"
+
+	"net"
+
+	//pb "buf.build/gen/go/jiangok/buf-hello/grpc-ecosystem/gateway/v2/album_list_service/v1/album_list_service/album_list_servicev1gateway"
+	gwpb "buf.build/gen/go/jiangok/buf-hello/grpc-ecosystem/gateway/v2/album_list_service/v1/album_list_service/album_list_servicev1gateway"
+	gpb "buf.build/gen/go/jiangok/buf-hello/grpc/go/album_list_service/v1/album_list_servicev1grpc"
+	pb "buf.build/gen/go/jiangok/buf-hello/protocolbuffers/go/album_list_service/v1"
+	"github.com/grpc-ecosystem/grpc-gateway/v2/runtime"
+
+	"google.golang.org/grpc"
+	"google.golang.org/grpc/credentials/insecure"
+)
+
+type server struct {
+	gpb.UnimplementedAlbumListServiceServer
+}
+
+func NewServer() *server {
+	return &server{}
+}
+
+func (s *server) GetAlbumList(ctx context.Context, in *pb.GetAlbumListRequest) (*pb.GetAlbumListResponse, error) {
+	log.Printf("Received request")
+	return &pb.GetAlbumListResponse{Id: "100", Title: "mytitle", Price: 10.00}, nil
+}
+
+// curl -k http://localhost:8090/v1/album_list_service
+func main() {
+	lis, err := net.Listen("tcp", ":8080")
+	if err != nil {
+		log.Fatalf("failed to listen: %v", err)
+	}
+	s := grpc.NewServer()
+	gpb.RegisterAlbumListServiceServer(s, &server{})
+
+	log.Printf("server listening at %v", lis.Addr())
+
+	go func() {
+		log.Fatalln(s.Serve(lis))
+	}()
+
+	conn, err := grpc.NewClient(
+		"0.0.0.0:8080",
+		grpc.WithTransportCredentials(insecure.NewCredentials()),
+	)
+	if err != nil {
+		log.Fatalln("Failed to dial server:", err)
+	}
+
+	gwmux := runtime.NewServeMux()
+	err = gwpb.RegisterAlbumListServiceHandler(context.Background(), gwmux, conn)
+	if err != nil {
+		log.Fatalln("Failed to register gateway:", err)
+	}
+
+	gwServer := &http.Server{
+		Addr:    ":8090",
+		Handler: gwmux,
+	}
+
+	log.Println("Serving gRPC-Gateway on http://0.0.0.0:8090")
+	log.Fatalln(gwServer.ListenAndServe())
+}
